5dc1d627b6fc92ba113320b7dc4e9ddf5b58328e
hamzaq453 <qhamza4532@gmail.com>
2025-10-29 18:59:12 +0500

    Add DiscountBanner component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import VerticalCallButton from "@/components/VerticalCallButton";
+import DiscountBanner from "@/components/DiscountBanner";
 
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DiscountBanner />
         <Navbar />
         {children}
         <Footer />
---
+++ src/components/DiscountBanner.tsx
@@ -0,0 +1,11 @@
+export default function DiscountBanner() {
+  return (
+    <div className="bg-deep-forest text-creamy-white py-3 px-4 relative border-t border-b border-[#2E3A24]">
+      <div className="max-w-7xl mx-auto text-center">
+        <p className="text-base sm:text-lg font-semibold">
+          SEASON START SPECIAL - GET 20% OFF ON ALL SERVICES
+        </p>
+      </div>
+    </div>
+  );
+}
